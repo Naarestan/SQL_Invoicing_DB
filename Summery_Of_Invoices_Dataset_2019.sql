d153b23a922b226ea655a0639a3b0d0f3c645eae
Nastaran Mahmoodzadeh <63141665+Naarestan@users.noreply.github.com>
2021-07-24 12:13:28 -0700

    Update Summery_Of_Invoices_Dataset_2019.sql

--- Summery_Of_Invoices_Dataset_2019.sql
+++ Summery_Of_Invoices_Dataset_2019.sql
@@ -6,8 +6,8 @@
 SELECT
 	'First Half of 2019' AS Date_Range,
 	SUM(invoice_total) AS Total_Sales,
-    SUM(payment_total) AS Total_Payments,
-    SUM(invoice_total - payment_total) AS What_we_expect
+    	SUM(payment_total) AS Total_Payments,
+  	SUM(invoice_total - payment_total) AS What_we_expect
 FROM invoices
 WHERE invoice_date BETWEEN '2019-01-01' AND '2019-06-30'
 
@@ -16,8 +16,8 @@ UNION
 SELECT
 	'Second Half of 2019' AS Date_Range,
 	SUM(invoice_total) AS Total_Sales,
-    SUM(payment_total) AS Total_Payments,
-    SUM(invoice_total - payment_total) AS What_we_expect
+	SUM(payment_total) AS Total_Payments,
+	SUM(invoice_total - payment_total) AS What_we_expect
 FROM invoices
 WHERE invoice_date BETWEEN '2019-07-01' AND '2019-12-31'
 
@@ -26,8 +26,8 @@ UNION
 SELECT
 	'Total' AS Date_Range,
 	SUM(invoice_total) AS Total_Sales,
-    SUM(payment_total) AS Total_Payments,
-    SUM(invoice_total - payment_total) AS What_we_expect
+	SUM(payment_total) AS Total_Payments,
+	SUM(invoice_total - payment_total) AS What_we_expect
 FROM invoices
 WHERE invoice_date BETWEEN '2019-01-01' AND '2019-12-31'
 

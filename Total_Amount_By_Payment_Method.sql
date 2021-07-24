fdecf871207e64f4083f3efe7b4f94bc0473320b
Nastaran Mahmoodzadeh <63141665+Naarestan@users.noreply.github.com>
2021-07-24 12:34:07 -0700

    Update Total_Amount_By_Payment_Method.sql

--- Total_Amount_By_Payment_Method.sql
+++ Total_Amount_By_Payment_Method.sql
@@ -1,6 +1,6 @@
 SELECT
 	pm.name AS Payment_method,
-    sum(p.amount) AS Total
+    	sum(p.amount) AS Total
 FROM payments as p
 JOIN payment_methods as pm
 	ON p.payment_method = pm.payment_method_id

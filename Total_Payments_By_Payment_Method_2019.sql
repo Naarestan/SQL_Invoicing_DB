158ec579cfdb318dcc9ad3cc410b4f03c76e8448
Nastaran Mahmoodzadeh <63141665+Naarestan@users.noreply.github.com>
2021-07-24 12:14:51 -0700

    Update Total_Payments_By_Payment_Method_2019.sql

--- Total_Payments_By_Payment_Method_2019.sql
+++ Total_Payments_By_Payment_Method_2019.sql
@@ -1,7 +1,7 @@
 SELECT
 	p.date AS Date,
-    pm.name AS Payment_Method,
-    sum(p.amount) AS Total_payments
+	pm.name AS Payment_Method,
+	sum(p.amount) AS Total_payments
 FROM payments AS p
 JOIN payment_methods AS pm
 	ON p.payment_method = pm.payment_method_id

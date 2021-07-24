41faef9eb6f9760fcb3b6f96e8c13b01c612f9f0
Nastaran Mahmoodzadeh <63141665+Naarestan@users.noreply.github.com>
2021-07-24 12:11:08 -0700

    Paid_more_than_$100_in_VA_State.sql

---
+++ Paid_more_than_$100_in_VA_State.sql
@@ -0,0 +1,19 @@
+SELECT
+	c.customer_id,
+    c.first_name,
+	c.last_name,
+    SUM(oi.quantity * oi.unit_price) AS Total_Sales
+FROM customers AS c
+JOIN orders AS o
+	USING(customer_id)
+JOIN order_items AS oi
+	USING(order_id)
+WHERE
+	state = 'VA'
+GROUP BY
+	c.customer_id,
+    c.first_name,
+	c.last_name
+HAVING
+	Total_Sales > 100;
+
